50b8fb08f3bbb86488312f5a2caff3b5d7e918b9
prasanna1521 <yagalakshmi1521@gmail.com>
2026-06-21 00:48:06 +0530

    Update script.js

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,21 +1,21 @@
-const display = document.getElementById("display");
+let display = document.getElementById("display");
 
-function appendValue(value) {
-    display.value += value;
+function append(value){
+  display.value += value;
 }
 
-function clearDisplay() {
-    display.value = "";
+function clearDisplay(){
+  display.value = "";
 }
 
-function deleteLast() {
-    display.value = display.value.slice(0, -1);
+function deleteLast(){
+  display.value = display.value.slice(0, -1);
 }
 
-function calculate() {
-    try {
-        display.value = eval(display.value);
-    } catch {
-        display.value = "Error";
-    }
+function calculate(){
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Error";
+  }
 }
